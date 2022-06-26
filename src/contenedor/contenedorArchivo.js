72b7a34addb1f452f8df92d19f30ba5e154fba3c
Leandro Possanzini <leandropossanzini@gmail.com>
2022-06-26 18:31:33 -0300

    creando la clase

--- src/contenedor/contenedorArchivo.js
+++ src/contenedor/contenedorArchivo.js
@@ -1,11 +1,31 @@
-
+let arrayDeProductos = []
 
 class Contenedor{
+    static id = 1
 
-    constructor(archivo){
-        this.archivo = archivo;
+    obtenerTodos(){
+        if(arrayDeProductos.length >= 1){
+            return arrayDeProductos
+        }else {
+            return []
+        }
     }
-
-    
+    obtenerPorId(id){
+        const productoBuscado = arrayDeProductos.find(elemento => elemento.id == id)
+        if(productoBuscado == undefined){
+            return {mensaje:"No se encontro el producto"}
+        } else {
+            return productoBuscado
+        }
+    }
+   existeElId(id){
+    const productoBuscado = arrayDeProductos.find(elemento => elemento.id == id)
+    if(productoBuscado == undefined){
+        return false
+    }else{
+        return true
+    }
+   }
+  
 
 }
