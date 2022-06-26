6669cd7fa294a7cc3831da7ada5e0102cf787f0b
Leandro Possanzini <leandropossanzini@gmail.com>
2022-06-26 18:49:17 -0300

    Terminado la clase Productos

--- src/contenedor/contenedorArchivo.js
+++ src/contenedor/contenedorArchivo.js
@@ -1,6 +1,6 @@
 let arrayDeProductos = []
 
-class Contenedor{
+class Productos{
     static id = 1
 
     obtenerTodos(){
@@ -26,6 +26,43 @@ class Contenedor{
         return true
     }
    }
-  
-
+  guardarProducto(producto){
+    if (producto.timestamp && producto.name && producto.descripcion && producto.code && producto.thumbnail && producto.price && producto.stock) {
+        producto.id = Productos.id;
+        arrayDeProductos.push(producto);
+        Productos.id++;
+        return producto;
+      } else {
+        return {mensaje:"Campo de producto faltante"}
+      }
+  }
+  actualizarPorId(id,producto){
+    const productoBuscado = arrayDeProductos.find(elemento => elemento.id == id)
+    if(productoBuscado == undefined){
+        return {mensaje:"el id no existe..."}
+    }else{
+        if (producto.timestamp && producto.name && producto.descripcion && producto.code && producto.thumbnail && producto.price && producto.stock) {
+            resultado.timestamp = producto.timestamp;
+            resultado.name = producto.name;
+            resultado.descripcion = producto.descripcion;
+            resultado.code = producto.code;
+            resultado.thumbnail = producto.thumbnail;
+            resultado.price = producto.price;
+            resultado.stock = producto.stock;
+          } else {
+            return {mensaje: "No ingreso todas las variables que tiene un producto..."}
+          }
+    }
+  }
+  borrarPorId(id){
+    const productoBuscado = arrayDeProductos.find(elemento => elemento.id != id)
+    if(productoBuscado == undefined){
+        return {mensaje: "no existe ese id"}
+    }else{
+        const productosNoEliminados = arrayDeProductos.filter(elemento => elemento.id != id)
+        productosNoEliminados.push(arrayDeProductos)
+    }
+  }
 }
+
+module.exports = {Productos}
